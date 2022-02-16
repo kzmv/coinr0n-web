54b58a13250f75f251086ee215a62d32987d7531
George Kouzmov <kouzmov@gmail.com>
2022-02-16 08:33:56 +0000

    update url

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,2 +1,2 @@
-export const baseUrl = "https://coinr0n-web-oz6yjxpan-kzmv.vercel.app/"
+export const baseUrl = "https://coinr0n-web.vercel.app/"
 export const baseBackendUrl = "https://coinr0n-api-e6c2.encoreapi.com/prod/"
